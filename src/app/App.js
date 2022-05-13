e2627af18cde9c835ec0a5189c404631ef6bc9fb
Sean Byrd <byrd.sean1015@gmail.com>
2022-05-13 18:27:28 -0400

    Set up project. Removing unnecessary files. Updated imports. Added basic html for gameboard.

---
+++ src/app/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div id="gameboard">
+      <div id="simon-game">
+        <div id="button-group">
+          <div class="button green" id="green"></div>
+          <div class="button red" id="red"></div>
+          <div class="button yellow" id="yellow"></div>
+          <div class="button blue" id="blue"></div>
+        </div>
+        <div id="game-controls">SIMON</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
