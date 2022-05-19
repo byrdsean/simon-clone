38b184ee0c44010ffe817870f3c20465b2d6320a
Sean Byrd <byrd.sean1015@gmail.com>
2022-05-18 21:52:10 -0400

    Setting up gameboard styling and DOM elements. Added workspace to the gitignore.

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,28 @@
 import "./App.css";
 
+const clickGameButton = () => console.log("Button clicked!");
+
 function App() {
   return (
     <div id="gameboard">
       <div id="simon-game">
         <div id="button-group">
-          <div class="button green" id="green"></div>
-          <div class="button red" id="red"></div>
-          <div class="button yellow" id="yellow"></div>
-          <div class="button blue" id="blue"></div>
+          <button
+            className="button green"
+            id="green"
+            onClick={clickGameButton}
+          ></button>
+          <button className="button red" id="red"></button>
+          <button className="button yellow" id="yellow"></button>
+          <button className="button blue" id="blue"></button>
         </div>
-        <div id="game-controls">SIMON</div>
+        <div id="game-controls">
+          <div id="control-panel">
+            <button id="startStop">Start/Stop</button>
+            <div id="difficulty">Medium</div>
+            <div id="score">Score: 1</div>
+          </div>
+        </div>
       </div>
     </div>
   );
